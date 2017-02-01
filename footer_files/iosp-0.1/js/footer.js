8a664ea20daa83f04d4696c03cf73337e82aa18b
Eric Koncina <mail@koncina.eu>
2017-02-01 09:02:38 +0100

    add footer demo

---
+++ footer_files/iosp-0.1/js/footer.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+            var footer = document.createElement('div');
+
+            footer.className = 'footer';
+
+            footer.innerHTML = 'Ceci est un test';
+            $("div").append(footer);
+        });
+    
+        
